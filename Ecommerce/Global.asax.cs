509c27ce9d31df5f38baa87b626d93322ea988e5
Wevertocouy <wevertoncouy@gmail.com>
2017-08-28 00:06:29 -0300

    classeDepartamentos

--- Ecommerce/Global.asax.cs
+++ Ecommerce/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -13,6 +14,7 @@ namespace Ecommerce
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<Models.EcommerceContext, Migrations.Configuration>());
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
